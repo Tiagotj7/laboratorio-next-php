d1c7d55f0ded742607132a44cd1463f6450c5413
Tiagotj7 <tiagowork07@gmail.com>
2026-05-29 19:07:45 -0300

    refactor: change return type of json method to never in Response class

--- server/src/Utils/Response.php
+++ server/src/Utils/Response.php
@@ -2,7 +2,7 @@
 namespace App\Utils;
 
 class Response {
-  public static function json($data, int $code = 200): void {
+  public static function json($data, int $code = 200): never {
     http_response_code($code);
     header("Content-Type: application/json; charset=utf-8");
     echo json_encode($data, JSON_UNESCAPED_UNICODE);
